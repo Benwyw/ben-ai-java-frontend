e1146d466e101a5069868721acaf8ae4b0478d7e
Ben <57297093+Benwyw@users.noreply.github.com>
2026-01-11 22:04:12 +0800

    update mdi-robot to logo.png

--- src/router/index.js
+++ src/router/index.js
@@ -335,7 +335,7 @@ const routes = [
         component: () => import('@/views/benkaneki/BenKanekiLanding.vue'),
         meta: {
           title: 'Ben Kaneki',
-          icon: 'mdi-robot',
+          iconImage: new URL('@/assets/logo.png', import.meta.url).href,
           navSection: 'benkaneki',
           navOrder: 1,
           defaultExpanded: true,
